17b31c2ea0f0e46ce9a6f6751f22604debf923ac
Sahil Kumar Sahu <39629334+sahil13kumar@users.noreply.github.com>
2020-08-12 12:30:38 +0530

    Create 4.Longest Increasing Subsequence.cpp

---
+++ Dynamic programming/4.Longest Increasing Subsequence.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int lis(int a[],int n){
+	
+	int lis[n];
+	lis[0] = 1;
+	int i,j;
+	for(i=1;i<n;i++){
+		lis[i] = 1;
+		for(j=0;j<i;j++){
+			
+			if(a[i] > a[j] && lis[i] < lis[j]+1)
+				lis[i] = lis[j]+1;
+		}
+	}
+	return *max_element(lis,lis+n);	
+}
+
+
+int main()  
+{  
+    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };  
+    int n = sizeof(arr)/sizeof(arr[0]);  
+    printf("Length of lis is %d\n", lis( arr, n ) );  
+  
+    return 0;  
+}
+
