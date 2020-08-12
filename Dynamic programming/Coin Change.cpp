00aec72500666b9b765d2bc59f6ed9a454c265f7
Sahil Kumar Sahu <39629334+sahil13kumar@users.noreply.github.com>
2020-08-12 12:04:05 +0530

    Update Coin Change.cpp

--- Dynamic programming/Coin Change.cpp
+++ Dynamic programming/Coin Change.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 
+
+/*
 int count(int set[],int n,int sum){
 	
 	int dp[n+1][sum+1];
@@ -35,5 +37,49 @@ int main()
     int m = sizeof(arr)/sizeof(arr[0]); 
     int n = 11; 
     cout<< count(arr, m, n); 
+    return 0; 
+} 
+*/
+
+
+
+#define MAX 9999
+int count(int coins[],int m, int amount){
+	
+	if(amount==0) return 0;
+	
+	int dp[m+1][amount+1];
+	int i,j;
+	for(i=0;i<=m;i++){
+		for(j=0;j<=amount;j++){
+			if(i==0 || j==0)
+				dp[i][j] = MAX;
+		}
+	}
+	
+	for(i=1;i<=m;i++){
+		for(j=1;j<=amount;j++){
+			if(j<coins[i-1])
+				dp[i][j] = dp[i-1][j];
+			else if(j == coins[i-1])
+				dp[i][j] = 1;
+			else
+				dp[i][j] = min(dp[i-1][j-coins[i-1]]+1,dp[i-1][j]);
+		}
+	}
+	
+	if(dp[m][amount]==MAX)
+		return -1;
+		
+	return dp[m][amount];
+}
+
+int main() 
+{ 
+    int i, j; 
+    int arr[] = {1, 2, 3}; 
+    int m = sizeof(arr)/sizeof(arr[0]); 
+    printf("%d ", count(arr, m, 4)); 
+    getchar(); 
     return 0; 
 } 
