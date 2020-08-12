762ee8b78e077b5ad143eb1b67fa3e5e35623dec
Sahil Kumar Sahu <39629334+sahil13kumar@users.noreply.github.com>
2020-08-12 09:38:03 +0530

    Create Subset_Sum.cpp

---
+++ Dynamic programming/Subset_Sum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include<iostream>	
+
+bool isSubsetSum(int set[],int n,int sum){
+	
+	bool subset[n+1][sum+1];
+	int i,j;
+	for(i=0;i<=n;i++){
+		subset[i][0]=true;
+	}
+	for(i=1;i<=sum;i++){
+		subset[0][i]=false;
+	}
+	
+	for(i=1;i<=n;i++){
+		for(j=1;j<=sum;j++){
+			
+			if(j < set[i-1])
+				subset[i][j] = subset[i-1][j];
+			if(j >= set[i-1])
+				subset[i][j] = subset[i-1][j] || subset[i-1][j-set[i-1]];
+		}
+	}
+	    
+	// uncomment this code to print table 
+	/*
+    for (int i = 0; i <= n; i++) 
+    { 
+       	for (int j = 0; j <= sum; j++) 
+          	printf ("%4d", subset[i][j]); 
+       	printf("\n"); 
+    }*/
+	return subset[n][sum];
+}
+
+int main() 
+{ 
+    int set[] = { 3, 34, 4, 12, 5, 2 }; 
+    int sum = 9; 
+    int n = sizeof(set) / sizeof(set[0]); 
+    if (isSubsetSum(set, n, sum) == true) 
+        printf("Found a subset with given sum"); 
+    else
+        printf("No subset with given sum"); 
+    return 0; 
+}
