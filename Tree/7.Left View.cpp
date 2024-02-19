3b767ffd3f988cc2990db92b41ceaf515bd0d624
Sahil Kumar Sahu <39629334+sahil13kumar@users.noreply.github.com>
2024-02-19 19:25:52 +0530

    Update 7.Left View.cpp

--- Tree/7.Left View.cpp
+++ Tree/7.Left View.cpp
@@ -54,3 +54,27 @@ void leftViewUtil(Node root)
     } 
 } 
 
+// Using Level Order Traversal
+void printLevelOrder(Node *root) 
+{ 
+	if(root==NULL)	return;
+	
+	queue<Node*> q;
+	q.push(root);
+	while(q.size()){
+		
+		int n = q.size();
+		
+		for(int i=0;i<n;i++){
+			Node* temp = q.front();
+			q.pop();
+			
+			if(i==0)
+				cout<<temp->data<<" ";
+			if(temp->left)
+				q.push(temp->left);
+			if(temp->right)
+				q.push(temp->right);
+		}
+	}
+} 
